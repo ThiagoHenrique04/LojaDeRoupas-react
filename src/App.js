bbc566a147f8bc57724b08fc8eb20820185a002f
thiagoBravox <thiago.gomes@bravox.io>
2023-04-14 18:18:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+import bolsaNikeGym from './static/bolsa nike gym club.jpeg';
+import camisaCity from './static/camisa city.jpeg';
+import camisaPalmeiras from './static/camisa-palmeiras.jpeg';
+import camisaEssentials from './static/camiseta essentials.jpeg';
+import camisaNikeBreathe from './static/camiseta nike breathe.jpeg';
+import gifDestaque from './static/gif-destaque.gif';
+import logo from './static/Logo.png';
+import short3Listras from './static/short 3 listras.jpeg';
+import tenisReinvention from './static/tenis reinvention.jpeg';
+import tenisZoomx from './static/tenis zoomx nike.jpeg';
+import tenisInfantilPuma from './static/tenis-infantil-puma.jpeg';
+import header from './static/header.jpg';
+import jaquetaTiro21 from './static/jaqueta tiro 21.jpeg';
+import { Body, CardWrapper9, CardWrapper8, CardWrapper7, CardWrapper6, CardWrapper5, CardWrapper4, TextPromocoes, TextNovidades, TextPopulares, CardWrapper3, CardWrapper2, CardWrapper1, GifImage, Entrar, Gif, Logo, Menu, Title } from './styles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu>
+        <Logo>
+          <img src={logo} className="App-logo" alt="logo" />
+        </Logo>
+        <Entrar href="https://wipsites.com.br">
+          Entrar
+        </Entrar>
+      </Menu>
+
+
+      <Body>
+
+        <Title>
+          <i><b> O conforto perfeito para o seu dia</b></i>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nibh ante, sagittis sed odio nec,
+            tristique tincidunt tellus. Mauris pellentesque nulla quis metus suscipit consequat. Proin libero sem,
+            semper sed eros et, aliquet dapibus lacus.
+          </p>
+
+
+        </Title>
+
+        <Gif>
+          <GifImage src={gifDestaque} className="App-logo" />
+        </Gif>
+
+        <TextPopulares>
+          <i>POPULARES</i>
+        </TextPopulares>
+
+
+        <CardWrapper1>
+
+          <img src={camisaPalmeiras} className="App-logo" alt="logo" />
+
+
+          <h3>Camisa Palmeiras 2023</h3>
+          <p>
+            <b>349,99</b>
+          </p>
+
+        </CardWrapper1>
+
+        <CardWrapper2>
+          <img src={bolsaNikeGym} className="App-logo" alt="logo" />
+
+          <h3>bolsa nike gym club</h3>
+          <p>
+            <b>329,99</b>
+          </p>
+
+        </CardWrapper2>
+
+
+        <CardWrapper3>
+
+          <img src={tenisInfantilPuma} className="App-logo" alt="logo" />
+
+          <h3>TENIS RS-X KIDS X PAW PATROL</h3>
+          <p>
+            <b>450,00</b>
+          </p>
+
+        </CardWrapper3>
+
+
+        <TextNovidades>
+          <i>NOVIDADES</i>
+        </TextNovidades>
+
+        <CardWrapper4>
+
+          <img src={camisaNikeBreathe} className="App-logo" alt="logo" />
+
+          <h3>camiseta nike breathe</h3>
+          <p>
+            <b>120,00</b>
+          </p>
+
+        </CardWrapper4>
+
+        <CardWrapper5>
+
+          <img src={tenisZoomx} className="App-logo" alt="logo" />
+
+          <h3>Tênis ZoomX Run Flyknit 3</h3>
+          <p>
+            <b>1499,99 </b>
+          </p>
+
+        </CardWrapper5>
+
+        <CardWrapper6>
+          <img src={jaquetaTiro21} className="App-logo" alt="logo" />
+          <h3>jaqueta tiro 21</h3>
+          <p>
+            <b>499,99</b>
+          </p>
+
+        </CardWrapper6>
+
+        <TextPromocoes>
+          <i>PROMOÇÕES</i>
+        </TextPromocoes>
+
+        <CardWrapper7>
+
+          <img src={camisaEssentials} className="App-logo" alt="logo" />
+
+          <h3>camiseta essentials</h3>
+          <p>
+            <s class="promo2">R$ 119,99 </s> <b>R$ 99,99 </b>
+          </p>
+
+        </CardWrapper7>
+
+        <CardWrapper8>
+
+          <img src={tenisReinvention} className="App-logo" alt="logo" />
+          <h3>tenis reinvention</h3>
+          <p>
+            <s className="promo1">R$ 1899,99 </s> <b>1699,99</b>
+          </p>
+
+        </CardWrapper8>
+
+        <CardWrapper9>
+          <img src={short3Listras} className="App-logo" alt="logo" />
+          <h3>short 3 listras</h3>
+          <p>
+            <s className="promo3">R$ 92,99 </s> <b>R$ 72,99 </b>
+          </p>
+
+        </CardWrapper9>
+
+
+
+
+
+      </Body>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,302 @@
+import styled from "styled-components";
+
+export const Body = styled.body`
+ 
+`;
+
+
+export const Menu = styled.header`
+position: relative;
+  width: 96%;
+  background-color: rgb(0, 0, 0);
+  height: 90px;
+  margin: -8px;
+  padding: 0 34px;
+  
+`;
+
+export const Logo = styled.div`
+position: absolute;
+  display: flex;
+  left: 30px;
+
+`;
+
+export const Entrar = styled.a`
+  color: antiquewhite;
+  position: relative;
+  top: 35px;
+  left: 1200px;
+  text-decoration: none;
+
+`;
+
+export const Title = styled.h1`
+  position: relative;
+  margin:30px;
+  font-size: 60px;
+  text-align: center;
+  font-family: "Roboto", 'Times New Roman';
+
+  p{
+  position: relative;
+  font-size: 25px;
+  margin:20px;
+  text-align: center;
+  font-family: "Roboto", 'Times New Roman';
+  overflow-x: hidden
+  }
+`;
+
+export const Gif = styled.div`
+ 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  animation-delay: initial;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  overflow-x: hidden
+`;
+
+export const GifImage = styled.img`
+  width: 100%;
+  height: 650px;
+ 
+`;
+
+export const TextPopulares = styled.h2`
+    top: 150px;
+    left: 50px;
+    font-size: 60px;
+    margin: 0px;
+    position: relative;
+`;
+
+
+export const CardWrapper1 = styled.button`
+  left: 100px;
+  padding: 10px;
+  top: 250px;
+  font-family: "Roboto";
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  cursor: pointer;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  transition: all 0.3s;
+  background: white;
+  border: 0px;
+  position: relative;
+  background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const CardWrapper2 = styled.button`
+ left: 200px;
+    padding: 10px;
+    top: 250px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const CardWrapper3 = styled.button`
+ left: 300px;
+    padding: 10px;
+    top: 250px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const TextNovidades = styled.h2`
+    top: 350px;
+    left: 50px;
+    font-size: 60px;
+    margin: 0px;
+    position: relative;
+
+`;
+
+export const CardWrapper4 = styled.button`
+    left: 100px;
+    padding: 10px;
+    top: 450px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const CardWrapper5 = styled.button`
+    left: 200px;
+    padding: 10px;
+    top: 450px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const CardWrapper6 = styled.button`
+    left: 300px;
+    padding: 10px;
+    top: 450px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const TextPromocoes = styled.h2`
+    top: 550px;
+    left: 50px;
+    font-size: 60px;
+    margin: 0px;
+    position: relative;
+
+`;
+
+export const CardWrapper7 = styled.button`
+    left: 100px;
+    padding: 10px;
+    top: 650px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const CardWrapper8 = styled.button`
+    left: 200px;
+    padding: 10px;
+    top: 650px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
+export const CardWrapper9 = styled.button`
+    left: 300px;
+    padding: 10px;
+    top: 650px;
+    font-family: "Roboto";
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    cursor: pointer;
+    -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    transition: all 0.3s;
+    background: white;
+    border: 0px;
+    position: relative;
+background-color: transparent;
+cursor: pointer;
+.card-item {
+  background: white;
+    color: rgb(8, 8, 8);
+    position: relative;
+}
+`;
+
